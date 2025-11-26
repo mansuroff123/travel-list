e8297fedecfaa01c8fcafb16bccdb1d1a53dd24b
mansuroff123 <mansuroff145@gmail.com>
2025-11-27 03:53:26 +0500

    onChange added for form and written form logic

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -20,19 +22,34 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(e);
+    if (!description) return;
+    const newItem = {description, quantity, packed: false, id: Date.now() };
+    console.log(newItem)
+
+    setDescription("")
+    setQuantity(1)
   }
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>😍 Sayohatingiz uchun nimalar kerak?</h3>
-      <select>
+      <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-          <option value={num} key={num}>{num}</option>
+          <option value={num} key={num}>
+            {num}
+          </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
@@ -43,7 +60,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} key={item.id}/>
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
