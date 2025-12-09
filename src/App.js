049859e738d5072155c83ab958f48ec20f945a47
mansuroff123 <mansuroff145@gmail.com>
2025-12-09 11:22:19 +0500

    sorting items

--- src/App.js
+++ src/App.js
@@ -77,10 +77,15 @@ function Form({ onAddItems }) {
 }
 
 function PackingList({ items, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems;
+  if (sortBy === 'input') sortedItems = items;
+  if (sortBy === 'description') sortedItems = items.slice().sort((a, b) => a.description.localeCompare(b.description))
+  if (sortBy === 'packed') sortedItems = items.slice().sort((a, b) => Number(a.packed) - Number(b.packed))
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             onDeleteItem={onDeleteItem}
@@ -89,6 +94,13 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
@@ -113,7 +125,9 @@ function Stats({ items }) {
   if (!items.length)
     return (
       <p className="stats">
-        <em>Boshlash uchun olib ketadigan narsalaringizni qo'shishingiz mumkun 🚀</em>
+        <em>
+          Boshlash uchun olib ketadigan narsalaringizni qo'shishingiz mumkun 🚀
+        </em>
       </p>
     );
   const numItems = items.length;
